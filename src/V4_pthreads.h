5da2d1b7e3dbe8dc8393ca715d9bc14f8923b8ed
panagiotou23 <alexandp@ece.auth.gr>
2020-12-02 19:07:44 +0200

    Fixed some bugs

--- src/V4_pthreads.h
+++ src/V4_pthreads.h
@@ -4,17 +4,12 @@
 
 //Create a struct in order to pass multiple arguments
 struct csc{
-    int M,
+    int start,  //the start of the first loop
+        stop,   //the end of the first loop
         * row,  //the CSC row vector
         * col;  //the CSC column vector
     float * c;  //the node vector that stores the triangles
 };
-
-//Create a mutex to avoid a data race
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-
-//Create a global int to keep track of the iteration 
-int iteration = 0;
 
 //Pthread's function
 void* find(void* arg){
@@ -22,13 +17,8 @@ void* find(void* arg){
     //Storing the argument in a variable
     struct csc * s = (struct csc * ) arg;
 
-    //Initializing the iteration index
-    pthread_mutex_lock(&m);
-    int i = iteration;
-    pthread_mutex_unlock(&m);
-
     //Using the same loop as V4 with different columns for each thread
-    while(i < s->M){
+    for(int i=s->start; i<s->start+s->stop; i++){
         for(int j=s->col[i]; j<s->col[i+1]; j++){
             int k=s->col[i];
             int l=s->col[s->row[j]];
@@ -44,10 +34,6 @@ void* find(void* arg){
                 }
             }
         }
-        pthread_mutex_lock(&m);
-        iteration++;
-        i = iteration;
-        pthread_mutex_unlock(&m);    
     }
     return NULL;
 }
@@ -56,17 +42,17 @@ long v4_pthreads(   int  * row, int * col,
                     float * c, int M, int nz, 
                     int num_threads)
 {
-
     //The Variables used to time the function
     struct timespec ts_start;
     struct timespec ts_end;
     
+
     //Initialization of c
     for(int i=0; i<M; i++) c[i] = 0;
 
     //Creating the thread array
     pthread_t threads[num_threads];
-
+    
     //Creating the struct array
     struct csc s[num_threads];
 
@@ -74,12 +60,30 @@ long v4_pthreads(   int  * row, int * col,
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
 
     //Initializing the struct array
-    for(int i=0; i<num_threads; i++){
-        s[i].c = c;
-        s[i].row = row;
-        s[i].col = col;
-        s[i].M = M;
-    }    
+    if(M%num_threads == 0){                     //If the number of Columns is multiple to the number of threads 
+        for(int i=0; i<num_threads; i++){
+            s[i].c = c;
+            s[i].row = row;
+            s[i].col = col;
+            s[i].stop = (M/num_threads);
+            s[i].start = (M/num_threads)*i;
+            
+        }    
+    }else{                                      //If not the residual will fall to the last thread
+        for(int i=0; i<num_threads-1; i++){
+            s[i].c = c;
+            s[i].row = row;
+            s[i].col = col;
+            s[i].stop = (M/num_threads);
+            s[i].start = (M/num_threads)*i;
+            
+        }
+        s[num_threads - 1].c = c;
+        s[num_threads - 1].row = row;
+        s[num_threads - 1].col = col;
+        s[num_threads - 1].start = (M/num_threads)*(num_threads - 1);
+        s[num_threads - 1].stop = (M/num_threads) + M%num_threads;        
+    }
 
     //Starting the threads
     for(int i=0; i<num_threads; i++) pthread_create(&threads[i], NULL, find, &s[i]);
