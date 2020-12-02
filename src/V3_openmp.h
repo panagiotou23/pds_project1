5bca2cff271792516ce210463f7afa47246bcf4e
panagiotou23 <alexandp@ece.auth.gr>
2020-12-02 20:05:38 +0200

    Fixed some bugs

--- src/V3_openmp.h
+++ src/V3_openmp.h
@@ -1,11 +1,8 @@
 #include <time.h>
-#include <stdio.h>
 
-#include <omp.h>
-
-//Find triangles with OpenMP
-long v3_openmp(  int * csc_col, int * csc_row,
-                            int * c, int M) 
+//Finds the triangles in the Adjacency Matrix stores them in c vector, returns the execution run-time
+long v3_openmp(    int * csc_row, int * csc_col,
+            int * c, int M) 
 {
     //The Variables used to time the function
     struct timespec ts_start;
@@ -14,33 +11,30 @@ long v3_openmp(  int * csc_col, int * csc_row,
     //Initialization of c
     for(int i=0; i<M; i++) c[i] = 0;
 
-    //Declaring the private variables of the OpenMP Parallelization
     int i,j,k,l;
 
     //Start the clock
     clock_gettime(CLOCK_MONOTONIC, &ts_start);
-
+    
     //Starting the OpenMP Parallelization
-    #pragma omp parallel shared(csc_row, csc_col) private(i, j, k, l)
+    #pragma omp parallel shared(csc_row, csc_col) private(i, j, k, l) 
     {
         //Parallelizing for
         #pragma omp for schedule(dynamic)
-        for(i=0; i<M; i++){
-            for(j=csc_col[i]; j<csc_col[i+1]; j++){
-                for(k=csc_col[csc_row[j]]; k<csc_col[csc_row[j] + 1]; k++){ 
-                    for(l=j+1; l<csc_col[i+1]; l++){
-                        if(csc_row[k] == csc_row[l]){
+        for(i=0; i<M; i++)
+            for(j=csc_col[i]; j<csc_col[i+1]; j++)
+                for(k=csc_col[csc_row[j]]; k<csc_col[csc_row[j] + 1]; k++)
+                    for(l=j+1; l<csc_col[i+1]; l++)
+                        if(csc_row[k] == csc_row[l])
                             c[i]++;
-                        } 
-                    }
-                }
-            }
-        }
-    }
-    
+                        
+                        
+    }           
+
     //Stop the clock
     clock_gettime(CLOCK_MONOTONIC, &ts_end);
 
     //Return the execution run-time
     return (ts_end.tv_sec - ts_start.tv_sec)* 1000000 + (ts_end.tv_nsec - ts_start.tv_nsec)/ 1000;
 }
+
